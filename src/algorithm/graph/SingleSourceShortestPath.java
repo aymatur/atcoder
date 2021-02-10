a9362dbf23b310f596ded369fde6a95af41aaead
ayumu <aym.matsuura@gmail.com>
2021-02-10 23:10:45 +0900

    http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A&lang=ja

--- src/algorithm/graph/SingleSourceShortestPath.java
+++ src/algorithm/graph/SingleSourceShortestPath.java
@@ -1,5 +1,10 @@
 package algorithm.graph;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class SingleSourceShortestPath {
@@ -11,8 +16,6 @@ public class SingleSourceShortestPath {
 		int V = scan.nextInt();
 		int E = scan.nextInt();
 		int r = scan.nextInt();
-
-		int[][] st = new int[V][V];
 
 		int[] s = new int[E];
 		int[] t = new int[E];
@@ -26,8 +29,57 @@ public class SingleSourceShortestPath {
 
 		scan.close();
 
+		List sMap = new ArrayList<HashMap<Integer, Integer>>();
 
+		for(int i = 0; i < V; i++) {
+			HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();
+			sMap.add(m);
+		}
 
+		for(int i = 0; i < E; i++) {
+			HashMap<Integer, Integer> m = (HashMap<Integer, Integer>) sMap.get(s[i]);
+			m.put(t[i], d[i]);
+		}
+
+		int[] ans = new int[V];
+		for(int i = 0; i < V; i++) {
+			ans[i] = Integer.MAX_VALUE;
+		}
+
+		int sum = 0;
+		List passed = new ArrayList<Integer>();
+		passed.add(r);
+
+		ans = solve(sMap, ans, r, sum, passed);
+
+		for(int i = 0; i < V; i++) {
+			if(i == r) {
+				System.out.println(0);
+			} else if(ans[i] == Integer.MAX_VALUE) {
+				System.out.println("INF");
+			} else {
+				System.out.println(ans[i]);
+			}
+		}
+
+	}
+
+	static int[] solve(List<Map<Integer,Integer>> m, int[] ans, int start, int sum, List a) {
+		Map<Integer, Integer> next = m.get(start);
+		Iterator<Integer> i = next.keySet().iterator();
+		while(i.hasNext()) {
+			Integer nextKey = i.next();
+			if(a.contains(nextKey)) {
+				continue;
+			}
+			a.add(nextKey);
+			sum = sum + next.get(nextKey);
+			if(ans[nextKey] > sum) ans[nextKey] = sum;
+			ans = solve(m, ans, nextKey, sum, a);
+
+		}
+
+		return ans;
 
 	}
 
