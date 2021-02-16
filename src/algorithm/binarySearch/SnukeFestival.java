e1e27e44a1bc3ee91f74c752db5a88056c195a5f
ayumu <aym.matsuura@gmail.com>
2021-02-16 23:59:08 +0900

    二分探索

--- src/algorithm/binarySearch/SnukeFestival.java
+++ src/algorithm/binarySearch/SnukeFestival.java
@@ -30,67 +30,46 @@ public class SnukeFestival {
 		Arrays.sort(b);
 		Arrays.sort(c);
 
+		int ans = countPattern(a, b, c, n);
 
-
+		System.out.print(ans);
 
 	}
 
 	static int countPattern(int[] a, int[] b, int[] c, int length) {
 
 		int count = 0;
-
+		if(a[0] >= b[length - 1]) return count;
+		if(b[0] >= c[length - 1]) return count;
 		for(int i = 0; i < length; i++) {
-			if(binarySearch(b, a[i]) == -1) continue;
-			int bIndex = binarySearch(b, a[i]);
+			if(a[i] >= b[length - 1]) continue;
+			int bIndex = binary_search(b, a[i]);
+			if(b[bIndex] >= c[length - 1]) continue;
 			for(int j = bIndex; j < length; j++) {
-				if(binarySearch(c, b[bIndex]) == -1) continue;
-				int cIndex = binarySearch(c, b[bIndex]);
-				count = count + (length - bIndex - 1)*(length - bIndex - 1);
+				int cIndex = binary_search(c, b[j]);
+				count = count + length - cIndex;
 			}
-
 		}
-
 		return count;
 	}
 
-	static int binarySearch(int[] b, int i) {
-
-		int result = 0;
-
-		int start = b.length%2 ==0 ? b.length/2 : (b.length+1)/2;
-
-		while(true) {
-			if(i > b[b.length-1]) {
-				result = -1;
-				break;
-			}
-			if(b[start] > i) {
-				if(b[start - 1] < i) {
-					result = start - 1;
-					break;
-				}else if(b[start - 1] == i ){
-					result = start;
-				}else {
-					start = start - start/2;
-				}
-			}
-			if(b[start] < i) {
-				if(b[start + 1] > i) {
-					result = start + 1;
-					break;
-				} else if(b[start + 1] == i) {
-					result = start + 2;
-				} else {
-					start = start + start/2;
-				}
-			}
-			if(b[start] == i) {
-				result = start + 1;
-				break;
-			}
-		}
-		return result;
-
+	static boolean isOK(int[] array, int index, int key) {
+	    if (array[index] > key) return true;
+	    else return false;
 	}
 
+
+	static int binary_search(int[] array, int key) {
+	    int left = -1;
+	    int right = array.length;
+
+	    while (right - left > 1) {
+	        int mid = left + (right - left) / 2;
+
+	        if (isOK(array, mid, key)) right = mid;
+	        else left = mid;
+	    }
+
+	    return right;
+	}
 }
