9e22824b010dedb2dc0e61a1b2ccba1c33aa2791
ayumu <aym.matsuura@gmail.com>
2021-02-20 02:25:27 +0900

    深さ優先探索基本アルゴリズム

---
+++ src/algorithm/graph/BasicDFS.java
@@ -0,0 +1,84 @@
+package algorithm.graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class BasicDFS {
+
+	static int[] first_order;
+	static int[] last_order;
+	static boolean[] seen;
+
+	static int first_ptr = 0;
+	static int last_ptr = 0;
+
+	public static void main(String[] args) {
+
+
+		Scanner scan = new Scanner(System.in);
+
+		int N = scan.nextInt();
+		int M = scan.nextInt();
+
+		ArrayList<List<Integer>> graph = new ArrayList<>();
+
+		for(int i = 0; i < N ; i++) {
+			graph.add(new ArrayList<Integer>(0));
+		}
+
+
+		for(int i = 0; i<M; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			graph.get(a).add(b);
+			graph.get(b).add(a);
+
+		}
+
+		scan.close();
+
+		System.out.println(Arrays.asList(graph).toString());
+
+		first_ptr = 0;
+		last_ptr = 0;
+
+		first_order = new int[N];
+		last_order = new int[N];
+
+		seen = new boolean[N];
+
+		dfs(graph, 0);
+
+		for(int i = 0; i < N ; i++) {
+			System.out.print(i+": "+first_order[i]+", "+last_order[i]);
+			System.out.println("");
+		}
+
+		System.out.println(Arrays.asList(graph).toString());
+
+
+	}
+
+	static void dfs(List<List<Integer>> graph, int v) {
+
+		first_ptr++;
+		first_order[v] = first_ptr;
+		System.out.println("first_order["+v+"]: "+", "+first_order[v]);
+		System.out.println("");
+		seen[v] = true;
+
+		for(Integer g : graph.get(v)) {
+			if(seen[g]) continue;
+			dfs(graph, g);
+		}
+
+		last_ptr++;
+		last_order[v] = last_ptr;
+		System.out.println("last_order["+v+"]: "+", "+last_order[v]);
+	}
+
+}
+
+
