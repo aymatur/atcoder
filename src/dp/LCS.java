a48c5eb7fb8efc4795da1a96c622c816cbd95ef2
ayumu <aym.matsuura@gmail.com>
2021-03-14 23:28:01 +0900

    編集距離途中

---
+++ src/dp/LCS.java
@@ -0,0 +1,19 @@
+package dp;
+
+import java.util.Scanner;
+
+public class LCS {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		String s = scan.next();
+		String t = scan.next();
+
+		scan.close();
+
+
+
+	}
+
+}
