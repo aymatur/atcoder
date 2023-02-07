e745625527220ffed4f8c95431938e75b67fb580
aymatur <aym.matsuura@gmail.com>
2023-02-07 22:15:52 +0900

    mitsui sumitomo 2019 b

--- 三井住友2019/d.cpp
+++ 三井住友2019/d.cpp
@@ -10,22 +10,22 @@ bool arr[999];
 int main() {
     cin >> N >> S;
 
-    for (int i = 0; i < 999; i++) arr[i] = false;
+    int ans = 0;
 
-    for (int i = 0; i < N - 2; i++) {
-        for(int j = i + 1; j < N - 1; j++) {
-            for(int k = j + 1; k < N; k++) {
-                string s;
-                if(S[i] != '0') s = {S[i],S[j],S[k]};
-                else s = {S[j],S[k]};
-                int index = stoi(s);
-                arr[index] = true;
+    for (int i = 0; i < 10; i++) {
+        if(S.find(to_string(i)) == string::npos) continue;
+        int ipos = S.find(to_string(i));
+        string nextString1 = S.substr(ipos+1);
+        for(int j = 0; j < 10; j++) {
+            if(nextString1.find(to_string(j)) == string::npos) continue;
+            int jpos = nextString1.find(to_string(j));
+            string nextString2 = nextString1.substr(jpos+1);
+            for(int k = 0; k < 10; k++) {
+                if(nextString2.find(to_string(k)) == string::npos) continue;
+                ans++;
             }
         }
     }
-    int ans = 0;
-    for (int i = 0; i < 999; i++) if(arr[i]) ans++;
     cout << ans << endl;
     return 0;
-
 }
