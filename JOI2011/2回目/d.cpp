3afbba870d613a2cd5f458d0310de8cadbadcb5d
aymatur <aym.matsuura@gmail.com>
2023-10-09 19:04:15 +0900

    JOI2011 d 2回目

--- JOI2011/2回目/d.cpp
+++ JOI2011/2回目/d.cpp
@@ -7,14 +7,14 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    int a[N];
+    vector<int> a(N);
     for (int i = 0; i < N; i++) cin >> a[i];
-    vector<vector<int>> dp(N, vector<int>(20, 0));
-    dp[0][a[0]]++;
+    vector<vector<long long>> dp(N, vector<long long>(21, 0));
+    dp[0][a[0]] = 1;
     for (int i = 1; i < N - 1; i++) {
         for (int j = 0; j <= 20; j++) {
-            if (j - a[i] >= 0 && dp[i-1][j] > 0) dp[i][j-a[i]]++;
-            if (j + a[i] <= 20 && dp[i-1][j] > 0) dp[i][j+a[i]]++;
+            if (j - a[i] >= 0) dp[i][j] += dp[i-1][j-a[i]];
+            if (j + a[i] <= 20) dp[i][j] += dp[i-1][j+a[i]];
         }
     }
     cout << dp[N-2][a[N-1]] << endl;
